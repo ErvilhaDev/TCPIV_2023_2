83515eec98515d549a13444b730fbf8aad52c4cc
Pedro Henrique Rosemback <rosembackpedrob>
2023-12-04 22:15:13 -0300

    Changelog v0.05 - Consertado alguns erros de commits e merge
    Updates:
    
    Merge feito com Build Vitor Inventário;
    Corrigido: andar com inventário aberto;
    
    Falta:
    Adicionar itens no InventoryContainer;
    estados do puzzle;
    classe do "desbloqueável" pelo puzzle;

---
+++ Scripts/Collectable.cs
@@ -0,0 +1,72 @@
+using Godot;
+using System;
+
+[Tool] 
+public class Collectable : Node2D
+{
+	[Export] CollectableData resource;
+	Area2D clickableArea;
+	InventoryManager inventoryManager;
+	Sprite itemSprite;
+	String itemName;
+	
+	[Export] NodePath itemSpritePath;
+	[Export] NodePath inventoryManagerPath;
+	[Export] NodePath clickableAreaPath;
+	
+	public override void _Ready()
+	{
+		//assigning Nodes
+		itemSprite = GetNode(itemSpritePath) as Sprite;
+		
+		inventoryManagerPath = "../../InventoryManager";
+		inventoryManager = GetNode(inventoryManagerPath) as InventoryManager;
+		//InventoryManager inventoryManager = (InventoryManager)GetNode("/root/InventoryManager");
+		
+		clickableArea = GetNode(clickableAreaPath) as Area2D;
+		
+		
+		//assigning Resource Data
+		itemSprite.Texture = resource.itemImage;
+		this.itemName = resource.itemName;
+		
+	}
+	
+	//With Build in Signal
+	//Collectable clicked Signal
+	private void _on_DetectableArea_input_event(object viewport, InputEvent @event, int shape_idx)
+	{
+		if(@event.IsActionPressed("click"))
+		{
+			GD.Print("Clicado... Aguarde o Personagem colidir com o Item");
+		}
+		
+	}
+	
+	//Collectable hovered Signal
+	private void _on_DetectableArea_mouse_entered()
+	{
+		GD.Print("Mouse por cima de coletável");
+	}
+	
+	//Collectable collision Signal
+	private void _on_DetectableArea_body_entered(Node body)
+	{	
+		if(!(body is PlayerControllerPedro))
+			return;
+		else
+			picked();
+	}
+	
+	
+	private void picked()
+	{
+		CallDeferred("free");
+		inventoryManager.addItem(this);
+	}
+	
+	public string getName()
+	{
+		return itemName;
+	}
+}
